20e29deaf6638ae638d7a17099842da456004836
Guilherme Assi Pereira <guiassi@hotmail.com>
2020-08-28 14:15:24 -0300

    Add files via upload

--- calculadora/js/main.js
+++ calculadora/js/main.js
@@ -2,7 +2,7 @@ let valor;
 let result;
 
 function btn(num){ //exibi os numeros na tela
-    valor = document.calc.visor.value += num;
+    valor = document.calc.visor.value += num; //concatena os números no click
 }
 
 function reset(){ //faz o botao C funfar
@@ -10,6 +10,6 @@ function reset(){ //faz o botao C funfar
 }
 
 function calculate(){ //faz as operações funfar
-    result = eval(valor);
+    result = eval(valor);// efetua todas as operações
     document.calc.visor.value = result.toLocaleString('pt-br');
 }
